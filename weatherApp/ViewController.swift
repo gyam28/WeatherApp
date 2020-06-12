a56b08aae1ab27ed8cc313da08b708ce85deaa3f
Gina <fogasgeorgina@gmail.com>
2020-06-12 10:57:12 +0300

    Initial Commit

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Gina on 12.6.2020.
+//  Copyright © 2020 Gina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
